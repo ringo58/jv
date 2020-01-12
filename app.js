022b1bcf7151962bc2307960c22cc67f2d29865b
ringo58 <mandom4sec@gmail.com>
2020-01-12 17:16:04 -0500

    Lmao

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 "use strict";
-var varivari = function (a) { return a; };
-console.log(varivari("hello there idiot"));
+var avenger = {
+    nombre: "spoderman",
+    clave: "peter parker",
+    poder: "arañas"
+};
+var nombre = avenger.nombre, clave = avenger.clave, poder = avenger.poder;
+console.log(nombre, clave, poder);
+var power = ["rojo", "azul", "amarillo", "verde", "negro"];
+var h1 = power[0], h2 = power[1], h3 = power[2], h4 = power[3], h5 = power[4];
+console.log(h1, h2, h3, h4, h5);
--- app.ts
+++ app.ts
@@ -1,2 +1,12 @@
-let varivari = a => a;
-console.log(varivari("hello there idiot"));
+let avenger =  {
+  nombre:"spoderman",
+  clave:"peter parker",
+  poder:"arañas"
+}
+let { nombre,clave,poder} = avenger;
+
+console.log(nombre, clave, poder);
+
+let power:string[] = ["rojo","azul","amarillo","verde","negro"];
+let [h1, h2, h3,h4,h5] = power;
+console.log(h1, h2, h3,h4,h5);
